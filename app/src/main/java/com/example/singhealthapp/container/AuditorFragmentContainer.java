83eb5dc5ba7fef87145653b259c4ffb79e9b4953
67Samuel <67ubersam@gmail.com>
2021-03-04 22:21:07 +0800

    add logout functionality

--- app/src/main/java/com/example/singhealthapp/container/AuditorFragmentContainer.java
+++ app/src/main/java/com/example/singhealthapp/container/AuditorFragmentContainer.java
@@ -1,8 +1,12 @@
 package com.example.singhealthapp.container;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.example.singhealthapp.FirebaseLogin;
 import com.example.singhealthapp.R;
 import com.example.singhealthapp.TestFragment;
 import com.example.singhealthapp.auditor.AddTenantFragment;
@@ -10,9 +14,11 @@ import com.example.singhealthapp.auditor.ReportsFragment;
 import com.example.singhealthapp.StatisticsFragment;
 import com.example.singhealthapp.auditor.SearchTenantFragment;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -20,12 +26,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AuditorFragmentContainer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String TAG = "AuditorFragmentContain";
+
     DrawerLayout auditor_drawer;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auditor_fragment_container);
+
+        mAuth = FirebaseAuth.getInstance();
 
         Toolbar auditor_toolbar = findViewById(R.id.auditor_toolbar);
         setSupportActionBar(auditor_toolbar);
@@ -43,14 +55,53 @@ public class AuditorFragmentContainer extends AppCompatActivity implements Navig
         }
     }
 
+//    @Override
+//    public void onBackPressed() {
+//        if (auditor_drawer.isDrawerOpen(GravityCompat.START)) {
+//            auditor_drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//        super.onBackPressed();
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return false;
+    }
+
     @Override
     public void onBackPressed() {
+        Log.d(TAG, "onBackPressed: ");
         if (auditor_drawer.isDrawerOpen(GravityCompat.START)) {
             auditor_drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setMessage("Do you want to log out? ");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    //AuditorFragmentContainer.super.onBackPressed();
+                    dialog.dismiss();
+                    mAuth.signOut();
+                    Intent intent = new Intent(AuditorFragmentContainer.this, FirebaseLogin.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                    //finish();
+                }
+            });
+            builder.show();
         }
-        super.onBackPressed();
     }
 
     @Override
--- app/src/main/java/com/example/singhealthapp/container/TenantFragmentContainer.java
+++ app/src/main/java/com/example/singhealthapp/container/TenantFragmentContainer.java
@@ -2,22 +2,32 @@ package com.example.singhealthapp.container;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.example.singhealthapp.FirebaseLogin;
 import com.example.singhealthapp.TestFragment;
 import com.example.singhealthapp.tenant.LatestReportFragment;
 import com.example.singhealthapp.tenant.MyReportsFragment;
 import com.example.singhealthapp.R;
 import com.example.singhealthapp.StatisticsFragment;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TenantFragmentContainer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "TenantFragmentContainer";
+
+    FirebaseAuth mAuth;
 
     DrawerLayout drawer;
 
@@ -25,6 +35,8 @@ public class TenantFragmentContainer extends AppCompatActivity implements Naviga
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tenant_fragmentcontainer);
+
+        mAuth = FirebaseAuth.getInstance();
 
         Toolbar toolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
@@ -42,14 +54,53 @@ public class TenantFragmentContainer extends AppCompatActivity implements Naviga
         }
     }
 
+//    @Override
+//    public void onBackPressed() {
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//        super.onBackPressed();
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return false;
+    }
+
     @Override
     public void onBackPressed() {
+        Log.d(TAG, "onBackPressed: ");
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setMessage("Do you want to log out? ");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    //AuditorFragmentContainer.super.onBackPressed();
+                    dialog.dismiss();
+                    mAuth.signOut();
+                    Intent intent = new Intent(TenantFragmentContainer.this, FirebaseLogin.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                    //finish();
+                }
+            });
+            builder.show();
         }
-        super.onBackPressed();
     }
 
     @Override
