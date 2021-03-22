e9563f4378639ba3580bd75524449eaae4772c03
chuaqibao <chuaqibao@gmail.com>
2021-03-22 22:20:06 +0800

    refactor login unit test code

--- app/src/test/java/com/example/singhealthapp/LoginUnitTest.java
+++ app/src/test/java/com/example/singhealthapp/LoginUnitTest.java
@@ -16,30 +16,29 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginUnitTest {
 
+    DatabaseApiCaller apiCaller = new Retrofit.Builder()
+            .baseUrl("https://esc10-303807.et.r.appspot.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(DatabaseApiCaller.class);
+
     // test api call using mock server (to simulate real response results)
     @Test
     public void mockPostLoginTest() throws IOException { // test if interface works
-        System.out.println("mockAuthCallTest starting...");
-
         String mockTokenJson = "{\"token\": \"127bc352b84eead35cc28340349a8dda9\"}";
 
-        // Create a MockWebServer. These are lean enough that you can create a new
-        // instance for every unit test.
         MockWebServer server = new MockWebServer();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(server.url("").toString())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
         server.enqueue(new MockResponse().setBody(mockTokenJson));
 
-        DatabaseApiCaller apiCaller = retrofit.create(DatabaseApiCaller.class);
+        DatabaseApiCaller mockApiCaller = new Retrofit.Builder()
+                .baseUrl(server.url("").toString())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(DatabaseApiCaller.class);
 
-        Call<Token> authCall = apiCaller.postLogin("auditor@test.com", "1234");
+        Call<Token> authCall = mockApiCaller.postLogin("auditor@test.com", "1234");
 
         String token = authCall.execute().body().getToken();
-        System.out.println("Token: " + token);
         assertEquals("127bc352b84eead35cc28340349a8dda9", token);
 
         // Shut down the server. Instances cannot be reused.
@@ -48,8 +47,6 @@ public class LoginUnitTest {
 
     @Test
     public void mockGetSingleUserTest() throws IOException {
-        System.out.println("mockAuthCallTest starting...");
-
         String mockSingleUserJson = "[{" +
                 "\"email\": \"auditor@test.com\", " +
                 "\"password\": \"pbkdf2_sha256$216000$Sz\", " +
@@ -59,36 +56,29 @@ public class LoginUnitTest {
                 "\"location\": \"\", " +
                 "\"institution\": \"SGH\", " +
                 "\"type\": \"Auditor\"}]";
-
         String mockToken = "127bc352b84eead35cc28340349a8dda9";
         String mockEmail = "auditor@test.com";
 
-        // Create a MockWebServer. These are lean enough that you can create a new
-        // instance for every unit test.
         MockWebServer server = new MockWebServer();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(server.url("").toString())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
         server.enqueue(new MockResponse().setBody(mockSingleUserJson));
 
-        DatabaseApiCaller apiCaller = retrofit.create(DatabaseApiCaller.class);
+        DatabaseApiCaller mockApiCaller = new Retrofit.Builder()
+                .baseUrl(server.url("").toString())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(DatabaseApiCaller.class);
 
-        Call<List<User>> authCall = apiCaller.getSingleUser("Token " + mockToken, mockEmail);
+        Call<List<User>> authCall = mockApiCaller.getSingleUser("Token " + mockToken, mockEmail);
 
         String userType = authCall.execute().body().get(0).getType();
 
-        System.out.println("User Type: " + userType);
         assertEquals("Auditor", userType);
 
-        // Shut down the server. Instances cannot be reused.
         server.shutdown();
     }
 //
 //    @Test
-//    public void mockTimeoutTest () {
+//    public void mockTimeoutTest () throws IOException {
 //
 //        MockWebServer server = new MockWebServer();
 //        Retrofit retrofit = new Retrofit.Builder()
@@ -115,77 +105,39 @@ public class LoginUnitTest {
 //        assert(authCall.isExecuted()).onLoadFailed(IOException.class);
 //    }
 
-
-
     // testing REST API with live Integration Tests (with JSON payload)
     @Test
     public void correctAuditorTest() throws IOException {
-        DatabaseApiCaller apiCaller = new Retrofit.Builder()
-                .baseUrl("https://esc10-303807.et.r.appspot.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(DatabaseApiCaller.class);
-
         Call<Token> authCall = apiCaller.postLogin("auditor@test.com", "1234");
         assertEquals(200, authCall.execute().code());
     }
 
     @Test
     public void correctTenantTest() throws IOException {
-        DatabaseApiCaller apiCaller = new Retrofit.Builder()
-                .baseUrl("https://esc10-303807.et.r.appspot.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(DatabaseApiCaller.class);
-
         Call<Token> authCall = apiCaller.postLogin("tenant@test.com", "1234");
         assertEquals(200, authCall.execute().code());
     }
 
     @Test
     public void wrongEmailTest() throws IOException {
-        DatabaseApiCaller apiCaller = new Retrofit.Builder()
-                .baseUrl("https://esc10-303807.et.r.appspot.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(DatabaseApiCaller.class);
-
         Call<Token> authCall = apiCaller.postLogin("xxxx@test.com", "1234");
         assertNotEquals(200, authCall.execute().code());
     }
 
     @Test
     public void wrongPasswordTest() throws IOException {
-        DatabaseApiCaller apiCaller = new Retrofit.Builder()
-                .baseUrl("https://esc10-303807.et.r.appspot.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(DatabaseApiCaller.class);
-
         Call<Token> authCall = apiCaller.postLogin("auditor@test.com", "xxxx");
         assertNotEquals(200, authCall.execute().code());
     }
 
     @Test
     public void emptyEmailTest() throws IOException {
-        DatabaseApiCaller apiCaller = new Retrofit.Builder()
-                .baseUrl("https://esc10-303807.et.r.appspot.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(DatabaseApiCaller.class);
-
         Call<Token> authCall = apiCaller.postLogin("", "xxxx");
         assertNotEquals(200, authCall.execute().code());
     }
 
     @Test
     public void emptyPasswordTest() throws IOException {
-        DatabaseApiCaller apiCaller = new Retrofit.Builder()
-                .baseUrl("https://esc10-303807.et.r.appspot.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(DatabaseApiCaller.class);
-
         Call<Token> authCall = apiCaller.postLogin("auditor@test.com", "");
         assertNotEquals(200, authCall.execute().code());
     }
