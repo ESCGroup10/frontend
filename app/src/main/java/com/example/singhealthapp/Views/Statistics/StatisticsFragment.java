2c40eb2632184948741e82a0b631ac4be73c50be
chuaqibao <chuaqibao@gmail.com>
2021-04-20 01:43:05 +0800

    statistics search bar done

--- app/src/main/java/com/example/singhealthapp/Views/Statistics/StatisticsFragment.java
+++ app/src/main/java/com/example/singhealthapp/Views/Statistics/StatisticsFragment.java
@@ -11,14 +11,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.singhealthapp.HelperClasses.CustomFragment;
 import com.example.singhealthapp.HelperClasses.EspressoCountingIdlingResource;
 import com.example.singhealthapp.Models.DatabaseApiCaller;
-import com.example.singhealthapp.HelperClasses.Ping;
+import com.example.singhealthapp.Models.Tenant;
 import com.example.singhealthapp.R;
 import com.google.android.material.tabs.TabLayout;
 
@@ -27,17 +28,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class StatisticsFragment extends CustomFragment {
 
-    private EditText tenantIdEditText;
+    private AutoCompleteTextView tenantIdEditText;
     private Button searchButton;
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
     private String tenantId, token, userType;
+    private ArrayList<String> tenantList;
+    private ArrayList<Integer> tenantIdList;
 
     private static List<TenantIdUpdateListener> listenerList = new ArrayList<>();
 
@@ -49,10 +55,7 @@ public class StatisticsFragment extends CustomFragment {
         getActivity().setTitle("View Statistics");
         View view = inflater.inflate(R.layout.f_stats, container, false);
 
-        tabLayout = view.findViewById(R.id.stats_tabLayout);
-        viewPager = view.findViewById(R.id.stats_viewPager);
-
-        getTabs();
+        getTabs(view);
         loadData();
 
         // create an api caller to the webserver
@@ -66,34 +69,12 @@ public class StatisticsFragment extends CustomFragment {
         searchButton = view.findViewById(R.id.searchTenantId_button);
 
         if (!userType.equals("Auditor")) {
-            tenantIdEditText.setVisibility(View.GONE);
-            searchButton.setVisibility(View.GONE);
-
-            new Thread(() -> {
-                try {
-                    Thread.sleep(500);
-                    tenantIdUpdate();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }).start();
-
-        } else { searchButton.setOnClickListener(v -> {
-
-            if (!(tenantIdEditText.length() == 0)) {
-                tenantId = tenantIdEditText.getText().toString();
-                try {
-                    tenantIdUpdate();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Toast.makeText(getActivity(), "Please fill in a tenant ID", Toast.LENGTH_SHORT).show();
-            }
-            System.out.println("Activity context: " + getContext());
-
-        }); }
-
+            setInvisibleView();
+            getTenantData();
+        } else {
+            getTenants();
+            setSearchButton();
+        }
         EspressoCountingIdlingResource.decrement();
         return view;
     }
@@ -106,27 +87,9 @@ public class StatisticsFragment extends CustomFragment {
     @Override
     public void onResume() {
         super.onResume();
-
         if (!userType.equals("Auditor")) {
-            new Thread(() -> {
-                try {
-                    Thread.sleep(500);
-                    tenantIdUpdate();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }).start();
+            getTenantData();
         }
-    }
-
-    private void getTabs() {
-        final StatsViewPagerAdapter statsViewPagerAdapter = new StatsViewPagerAdapter(getChildFragmentManager());
-
-        statsViewPagerAdapter.addFragment(ReportStatsFragment.getInstance(), "REPORT");
-        statsViewPagerAdapter.addFragment(TotalScoreStatsFragment.getInstance(), "TOTAL");
-
-        viewPager.setAdapter(statsViewPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
     }
 
     public interface TenantIdUpdateListener {
@@ -155,4 +118,93 @@ public class StatisticsFragment extends CustomFragment {
         tenantId = String.valueOf(sharedPreferences.getInt("USER_ID_KEY", 0));
     }
 
+    private void getTenantData() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+                tenantIdUpdate();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void getTenants() {
+        Call<List<Tenant>> call = apiCaller.getTenant("Token " + token);
+            call.enqueue(new Callback<List<Tenant>>() {
+                @Override
+                public void onResponse(Call<List<Tenant>> call, Response<List<Tenant>> response) {
+                    if (response.code() == 200) {
+                        parseTenants(response);
+                        setAdapter();
+                    }
+                    System.out.println("Response code is " + response.code());
+                }
+                @Override
+                public void onFailure(Call<List<Tenant>> call, Throwable t) {
+                    Toast.makeText(getActivity(), "Failure: " + t.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+    }
+
+    private void parseTenants(Response<List<Tenant>> response) {
+        List<Tenant> responseBody = response.body();
+        tenantIdList = new ArrayList<>();
+        tenantList = new ArrayList<>();
+        for (Tenant t : responseBody) {
+            tenantList.add(t.getName() + " " + t.getId());
+            tenantIdList.add(t.getId());
+        }
+
+        System.out.println("" + tenantList);
+    }
+
+    private void setAdapter() {
+        System.out.println("Activity: " + getActivity());
+        System.out.println("Activity: " + getContext());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, tenantList);
+        tenantIdEditText.setAdapter(adapter);
+    }
+
+    private void setSearchButton() {
+        searchButton.setOnClickListener(v -> {
+            if (!(tenantIdEditText.length() == 0)) {
+                searchQuery();
+                try {
+                    tenantIdUpdate();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(getActivity(), "Please fill in a tenant ID", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void searchQuery() {
+        String tenantQuery = tenantIdEditText.getText().toString();
+        if (tenantList.contains(tenantQuery)) {
+            tenantId = String.valueOf(tenantIdList.get(tenantList.indexOf(tenantQuery)));
+        }
+    }
+
+    private void getTabs(View view) {
+        tabLayout = view.findViewById(R.id.stats_tabLayout);
+        viewPager = view.findViewById(R.id.stats_viewPager);
+
+        final StatsViewPagerAdapter statsViewPagerAdapter = new StatsViewPagerAdapter(getChildFragmentManager());
+
+        statsViewPagerAdapter.addFragment(ReportStatsFragment.getInstance(), "REPORT");
+        statsViewPagerAdapter.addFragment(TotalScoreStatsFragment.getInstance(), "TOTAL");
+
+        viewPager.setAdapter(statsViewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setInvisibleView() {
+        tenantIdEditText.setVisibility(View.GONE);
+        searchButton.setVisibility(View.GONE);
+    }
 }
