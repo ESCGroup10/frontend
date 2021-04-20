6ea1907ed5a83d04fa2543e28afab879887a8e2c
67Samuel <67ubersam@gmail.com>
2021-04-20 12:38:47 +0800

    -

--- app/src/main/java/com/example/singhealthapp/Containers/TenantFragmentContainer.java
+++ app/src/main/java/com/example/singhealthapp/Containers/TenantFragmentContainer.java
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.content.Intent;
